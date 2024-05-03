79a144ccf019f0a9d6bfddeb8d76b04d91a34854
Austin Frick <austin.frick429@gmail.com>
2024-05-02 21:09:01 -0400

    first commit

---
+++ outing_repo/outing.cs
@@ -0,0 +1,13 @@
+﻿namespace KomodoOuting 
+{
+    public enum EventType{Golf =1, Bowling, AmusementPark, Concert }
+public class Outing
+{
+public string EventType { get; set; } 
+public int NumberOfAttendees { get; set; } 
+public DateTime Date { get; set; }
+public decimal CostPerPerson { get; set; } 
+public decimal TotalCost { get; set; }
+}
+}
+
---
+++ outing_repo/outing_repo.cs
@@ -0,0 +1,83 @@
+using System.Collections.Generic;
+using System;
+namespace KomodoOuting 
+{
+    public class OutingRepo 
+    {
+        private List<Outing> _outings = new List<Outing>() ;
+
+        //Create 
+        public void AddOuting(Outing outing) 
+        {
+            _outings.Add(outing) ;
+        } 
+
+        //Read
+        public Outing GetOutingByDate(DateTime date) 
+        {
+            foreach(Outing outing in _outings) 
+            {
+                if(outing.Date.Date == date.Date) 
+                {
+                    return outing; 
+                }
+            }
+            return null ;
+        } 
+   
+    
+
+        //Update not used in the challenge but here is what it might look like
+        // public void Updateouting(DateTime date, Outing updatedOuting) 
+        // {
+        // foreach(Outing outing in _outings) 
+        // {
+        // if(outing.Date.Date == date.Date) 
+        // {
+        //     outing.EventType = updatedOuting.EventType;
+        //     outing.NumberOfAttendees = updatedOuting.NumberOfAttendees;
+        //     outing.Date = updatedOuting.Date;
+        //     outing.CostPerPerson = updatedOuting.CostPerPerson;
+        //     outing.TotalCost = updatedOuting.TotalCost;
+        //     break;
+        // }
+        // }
+        // } 
+
+        //Delete not used in the challenge, but here is what it might look like 
+        // public void DeleteOuting(Outing outing) 
+        // {
+        //     _outings.Remove(outing) ;
+        // } 
+
+        //Calculate combined cost for all outings 
+        public decimal CalculateCombinedCost() 
+        {
+            decimal TotalCost = 0;
+            foreach(Outing outing in _outings) 
+            {
+                TotalCost += outing.TotalCost;
+            } 
+            return TotalCost;
+        }
+    //Calculate outing costs by type
+    public Dictionary<string, decimal> CalculateCostByType() 
+    {
+        var costByType = new Dictionary<string, decimal>() ;
+        foreach(var outing in _outings) 
+        {
+            if (costByType.ContainsKey(outing.EventType))
+            {
+                costByType[outing.EventType] += outing.TotalCost;
+            } 
+            else
+            {
+                costByType.Add(outing.EventType, outing.TotalCost) ;
+            }
+        } 
+        return costByType;
+    }
+    } 
+
+
+}
